da1761f82ae7f46d76f871e36585857b79fc72c9
Glitch (lab08-webapp-2023) <none>
2023-03-20 13:05:12 +0000

    👂⛽ Updated with Glitch

--- controllers/about.js
+++ controllers/about.js
@@ -1,9 +1,9 @@
 'use strict';
 
 // import all required modules
-const logger = require('../utils/logger');
-const developerStore = require('../models/developer-store.js');
-const accounts = require ('./accounts.js');
+import logger from '../utils/logger.js';
+import developerStore from '../models/developer-store.js';
+import accounts from './accounts.js';
 
 // create about object
 const about = {
@@ -25,4 +25,4 @@ const about = {
 };
 
 // export the about module
-module.exports = about;
+export default about;
--- models/user-store.js
+++ models/user-store.js
@@ -1,28 +1,30 @@
 'use strict';
 
-const _ = require('lodash');
-const JsonStore = require('./json-store');
+import logger from '../utils/logger.js';
+import JsonStore from './json-store.js';
 
 const userStore = {
 
-  store: new JsonStore('./models/user-store.json', {users: []}),
+  store: new JsonStore('./models/user-store.json', { users: [] }),
   collection: 'users',
 
   getAllUsers() {
     return this.store.findAll(this.collection);
   },
-
-  addUser(user) {
-    this.store.add(this.collection, user);
-  },
-
+  
   getUserById(id) {
-    return this.store.findOneBy(this.collection, { id: id });
+    return this.store.findOneBy(this.collection, (user => user.id === id));
   },
-
+  
   getUserByEmail(email) {
-    return this.store.findOneBy(this.collection, { email: email });
+    return this.store.findOneBy(this.collection, (user => user.email === email));
   },
-}
+  
+  addUser(user) {
+    this.store.addCollection(this.collection, user);
+  },
 
-module.exports = userStore;
+
+};
+
+export default userStore;
